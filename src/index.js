1ab983b6cc23ee329134eb626a9be3248576ce3e
rikuimm <rikuimm@metropolia.fi>
2022-01-17 18:31:37 +0200

    add json menu

--- src/index.js
+++ src/index.js
@@ -1,21 +1,8 @@
-const coursesEn = [
-  "Hamburger, cream sauce and poiled potates",
-  "Goan style fish curry and whole grain rice",
-  "Vegan Chili sin carne and whole grain rice",
-  "Broccoli puree soup, side salad with two napas",
-  "Lunch baguette with BBQ-turkey filling",
-  "Cheese / Chicken / Vege / Halloum burger and french fries",
-];
-const coursesFi = [
-  "Jauhelihapihvi, ruskeaa kermakastiketta ja keitettyä perunaa",
-  "Goalaista kalacurrya ja täysjyväriisiä",
-  "vegaani Chili sin carne ja täysjyväriisi",
-  "Parsakeittoa,lisäkesalaatti kahdella napaksella",
-  "Lunch baguette with BBQ-turkey filling",
-  "Juusto / Kana / Kasvis / Halloumi burgeri ja ranskalaiset",
-];
+import lunchMenuRaw from './menu.json';
 
+const lunchMenu = lunchMenuRaw.courses;
 const menu = document.querySelector('#menu');
+let currentLanguage = 'en';
 
 const clearMenuList = (parent) => {
   while (parent.firstChild) {
@@ -23,56 +10,65 @@ const clearMenuList = (parent) => {
   }
 };
 
-const writeMenuList = (items, parent) => {
+const writeMenuList = (items, lang, parent) => {
   clearMenuList(parent);
 
-  for (const item of items) {
-    const menuItem = document.createElement('li');
-    menuItem.textContent = item;
-    parent.appendChild(menuItem);
+  for (let i = 1; i < Object.keys(items).length + 1; i++) {
+    let title;
+    if (lang === 'en') {
+      title = items[i].title_en;
+    } else if (lang == 'fi') {
+      title = items[i].title_fi;
+    }
+    const properties = items[i].properties;
+    const price = items[i].price;
+    const menuListItem = document.createElement('li');
+    menuListItem.innerHTML =
+      '<strong>' + title + '</strong> (' + properties + ')<br>' + price;
+    parent.appendChild(menuListItem);
   }
 };
 
-writeMenuList(coursesEn, menu);
+writeMenuList(lunchMenu, currentLanguage, menu);
 
 const changeLanguage = document.querySelector('#changeLanguage');
-let currentLanguage = 'en';
 
 changeLanguage.addEventListener('click', () => {
   if (currentLanguage === 'fi') {
     currentLanguage = 'en';
-    writeMenuList(coursesEn, menu);
+    writeMenuList(lunchMenu, currentLanguage, menu);
   } else if (currentLanguage === 'en') {
     currentLanguage = 'fi';
-    writeMenuList(coursesFi, menu);
+    writeMenuList(lunchMenu, currentLanguage, menu);
   }
 });
 
 const sortMenu = (menu, asc) => {
-  if(asc) {
-    return menu.sort((a, b) => {
-      return a.localeCompare(b);
-    });
-  } else {
-    return menu.sort((a, b) => {
-      return b.localeCompare(a);
-    });
+  const menuItems = menu.childNodes;
+  const array = [];
+  for (const item of menuItems) {
+    array.push(item.innerHTML);
   }
+
+  if (asc) {
+    array.sort();
+  } else {
+    array.sort((a, b) => b - a);
+  }
+
+  clearMenuList(menu);
+
+  array.forEach((item) => {
+    const menuListItem = document.createElement('li');
+    menuListItem.innerHTML = item;
+    menu.appendChild(menuListItem);
+  });
 };
 
-const sort = document.querySelector('#sort');
 let asc = true;
 
-const getCurrentMenu = (menu) => {
-  const array = [];
-  for (childNode of menu.childNodes) {
-    array.push(childNode.textContent);
-  }
-  return array;
-};
-
 sort.addEventListener('click', () => {
-  writeMenuList(sortMenu(getCurrentMenu(menu), asc), menu);
+  sortMenu(menu, asc);
   asc = asc ? false : true;
 });
 
@@ -81,9 +77,11 @@ const random = document.querySelector('#getRandom');
 const getRandomItem = (menu) => {
   const index = Math.floor(Math.random() * menu.childNodes.length);
   const randomItem = menu.childNodes[index];
-  return randomItem.textContent;
+  return randomItem.innerHTML;
 };
 
 random.addEventListener('click', () => {
-  document.querySelector('#random').innerHTML = '<strong>Your random dish:</strong><br>' + getRandomItem(menu);
+  document.querySelector('#random').innerHTML =
+    '<h2>Your random dish:</h2>' + getRandomItem(menu);
+  document.location = '#random';
 });
